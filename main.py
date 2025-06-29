7f99d510c8c3222229840a24ac2e6aa635ee6ce7
StefanMarston <stefankilber@t-online.de>
2025-06-29 02:37:41 +0200

    Added a hue security layer: Passwords are now en/decoded. This will prevenmt someone ebign able to read the password from the filesystem.json file.

--- main.py
+++ main.py
@@ -1492,6 +1492,7 @@ def first_boot_setup():
         # Encode password before storing
         encoded_pw = encode_password_custom(password)
 
+
         # Add new user with encoded password
         USERS[username] = {"password": encoded_pw}
 
